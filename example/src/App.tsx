d2c173c6d65b3061aa44e2cceff9deed7f8ff87b
Nikolay Lapa <lapanicholas@gmail.com>
2022-04-19 15:07:32 +0300

    Clean up (#5)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,30 +4,6 @@ import {Navigator} from './Navigator'
 import {TipProvider} from 'react-native-tooltip-overhaul'
 
 const App: FC = () => {
-  // const renderItem: ListRenderItem<number> = useCallback(
-  //   ({index}) => {
-  //     if (index % 3 === 0) {
-  //       return (
-  //         <Tip
-  //           renderTip={renderTip}
-  //           style={{margin: 10, alignSelf: 'flex-start'}}>
-  //           <Text>Yoooo</Text>
-  //         </Tip>
-  //       )
-  //     }
-  //     return (
-  //       <View
-  //         style={{
-  //           width: 50,
-  //           height: 50,
-  //           margin: 10,
-  //           backgroundColor: index % 2 ? 'red' : 'green',
-  //         }}></View>
-  //     )
-  //   },
-  //   [renderTip],
-  // )
-
   return (
     <TipProvider>
       <Navigator />
